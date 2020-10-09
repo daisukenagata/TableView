453b18acd8e80bc9ff35473178a387f25d7aa6e0
dbank0208@gmail.com <dbank0208@gmail.com>
2020-10-09 12:00:00 +0900

    add

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -12,31 +12,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var table: UITableView!
     
     private let tex = ["1",
-                       "2",
-                       "333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333433333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333334",
+                       "22",
+                       "333",
                        "444444444444444444444444444444444444444444",
-                       "5",
-                       "6",
-                       "7",
-                       "8",
-                       "9",
+                       "55555",
+                       "666666",
+                       "7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777",
+                       "8888888888",
+                       "999999999999",
                        "10"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         table.dataSource = self
         table.delegate = self
-        table.rowHeight = 44
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        table.reloadData()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableCell
         cell.tableLabel.text = tex[indexPath.row]
         cell.tableLabel.sizeToFit()
+        
+        cell.backLabel.text = tex[indexPath.row]
+        cell.backLabel.sizeToFit()
+        cell.backLabel.textColor = .clear
+
         table.rowHeight = cell.tableLabel.frame.size.height >= 44 ? cell.tableLabel.frame.size.height : 44
         return cell
     }
